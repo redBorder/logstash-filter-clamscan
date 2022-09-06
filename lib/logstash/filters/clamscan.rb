dcd6ab0fdccc3b4f84cc38e32cafd73aa757bd4b
eareyes <eareyes@redborder.com>
2022-09-06 13:52:51 +0100

    Fix typo

--- lib/logstash/filters/clamscan.rb
+++ lib/logstash/filters/clamscan.rb
@@ -123,7 +123,7 @@ class LogStash::Filters::Clamscan < LogStash::Filters::Base
     event.set(@target, clamscan_result)
     event.set(@score_name, score)
 
-    AerospikeMethods::update_malware_hash_score(@aerospike, @aerospike_namespace, @aerospike_set, @hash, @score_name, score, "fb")
+    AerospikeManager::update_malware_hash_score(@aerospike, @aerospike_namespace, @aerospike_set, @hash, @score_name, score, "fb")
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
